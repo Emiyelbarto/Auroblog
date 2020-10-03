aa97356b4680deb026bee1e9f64dc697f1f5bbf0
Emiliano Perez <emiyelbarto@hotmail.com>
2020-10-02 20:46:12 -0500

    template para la pagina del admin

--- auroblog-front/pages/admin/index.vue
+++ auroblog-front/pages/admin/index.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>admin page</div>
+  <div class="py-10 px-auto">
+    <div class="">
+      <h1>Admin Page</h1>
+    </div>
+    <div>
+      <p>Pagina para editar blogs siendo admin</p>
+      <b-table striped hover :items="items"></b-table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
+  data() {
+    return {
+      items: [
+        {
+          blog: 'title',
+          content: 'content',
+          created_at: 'date',
+          action: 'icon',
+        },
+        {
+          blog: 'title',
+          content: 'content',
+          created_at: 'date',
+          action: 'icon',
+        },
+        {
+          blog: 'title',
+          content: 'content',
+          created_at: 'date',
+          action: 'icon',
+        },
+        {
+          blog: 'title',
+          content: 'content',
+          created_at: 'date',
+          action: 'icon',
+        },
+      ],
+    }
+  },
+  computed: {},
 }
 </script>
